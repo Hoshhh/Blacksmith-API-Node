15db26eb0c54eb21ab24178208502ee50de0e779
Hoshhh <nyxious123@live.com>
2023-07-17 14:29:08 -0400

    basic validation test

--- src/models/Item.ts
+++ src/models/Item.ts
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
 const ItemSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    description: String,
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        maxlength: [20, 'name can not be more than 20 characters']
+    },
+    type: {
+        type: String,
+        required: [true, 'must provide item type'],
+        maxlength: [15, 'type can not be more than 15 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'must provide item description'],
+        maxlength: [150, 'item description can not be more than 150 characters']
+    },
 })
 
 export const Item = mongoose.model('Item', ItemSchema)
